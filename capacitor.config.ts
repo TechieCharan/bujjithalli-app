2aa170afe89631c0e09228780274e5c23c186aaa
charanteja <pasagadugulacharanteja@gmail.com>
2026-05-21 22:49:11 +0530

    some for android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bujjithalli.app',
+  appName: 'Bujjithalli Productivity',
+  webDir: 'dist'
+};
+
+export default config;
